ee85cf804c9220f28f69594364bf9a1a4f7ff9d2
knight4nero-dotcom <knight4nero@gmail.com>
2026-07-02 12:51:46 -0700

    Built web
    Branch: main
    Commit: e3aa32d084f28ed0d809748ca1a26a3fc8b7d6d5
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2503586501" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "715479837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
